5f05497ce84cf1b5d71e2fddb299dcaec239ab3b
张博 <zhangbo3@yy.com>
2014-11-07 17:51:34 +0800

    Add assert in testing cases

--- test/test_graph.c
+++ test/test_graph.c
@@ -1,4 +1,5 @@
 #include "../src/all.h"
+#include <assert.h>
 
 void __callback(struct graph_node_link_t *link){
     printf("[DEBUG]link %s \n", link->node_name);
@@ -15,10 +16,15 @@ int main(){
     struct graph_node_t * node3 = create_node(node_3_name, NULL, 0);
     struct graph_node_t * node4 = create_node(node_4_name, NULL, 0);
 
+    assert(node1->link_num == 0);
+
     printf("[DEBUG] create link with between two node\n");
+
     build_link(node1, node2);
     build_link(node1, node3);
     build_link(node1, node4);
+
+    assert(node1->link_num == 3);
 
     printf("[DEBUG] node %s with link %ld\n", node1->node_name, node1->link_num);
     rb_link_tree_iterate(node1, __callback);
@@ -28,13 +34,18 @@ int main(){
     );
 
     destory_link(node1, node2);
+
+    assert(node1->link_num == 2);
+
     rb_link_tree_iterate(node1, __callback);
     printf("[DEBUG] node %s with link %ld\n", node1->node_name, node1->link_num);
-
     printf("[DEBUG] destory link between %s <--> %s\n",
         node1->node_name, node3->node_name
     );
     destory_link(node1, node3);
+
+    assert(node1->link_num == 1);
+
     rb_link_tree_iterate(node1, __callback);
     printf("[DEBUG] node %s with link %ld\n", node1->node_name, node1->link_num);
 
